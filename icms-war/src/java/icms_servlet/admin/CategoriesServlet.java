f1c1cc8121021796c5a968c99f2936cc0eb1429e
unknown <Chouchou@.(none)>
2009-11-14 15:39:33 +0100

    Test avec heritage 2

---
+++ icms-war/src/java/icms_servlet/admin/CategoriesServlet.java
@@ -0,0 +1,94 @@
+package icms_servlet.admin;
+
+import icms_ejb.*;
+import icms_servlet.*;
+import java.io.IOException;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class CategoriesServlet extends HttpServlet {
+
+    @EJB
+    private GestionnairePagesLocal gestionnaireArticles;
+    @EJB
+    private GestionnaireUsersLocal gestionnaireUsers;
+    // Not EJB
+    private String page;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        if (!Security.checkUserIsAuthenticated(request, response, gestionnaireUsers)) {
+            return;
+        }
+
+        // Priority for the action parameter passed by the page, not by the servlet config
+        int action = request.getParameter("action") != null ? Integer.parseInt(request.getParameter(
+                "action")) : getServletConfig().getInitParameter("action") != null ? Integer.parseInt(getServletConfig().
+                getInitParameter("action")) : -1;
+
+        switch (action) {
+            case Config.INDEX:
+              
+                page = "admin/categories.jsp"; // render
+                break;
+
+            case Config.CREATE:
+
+            
+              
+                    gestionnaireArticles.createCategory((String) request.getParameter("title"), (String) request.getParameter("permalink"), (String) request.getParameter("intro"), (String) request.getParameter("content"));
+             
+                response.sendRedirect("/icms-war/articles");
+                return;
+
+            default:
+                // gestionnaireArticles.createCategory("essai 1", "essai1", "blabla", "essai1");
+                // gestionnaireArticles.createSection("essai 1", "essai1", "blabla", "essai1", gestionnaireArticles.findCategoryByTitle("essai 1"));
+                  page = "admin/categories.jsp"; // render
+                break;
+        }
+
+        RequestDispatcher dp = request.getRequestDispatcher("/" + page);
+        dp.forward(request, response);
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ icms-war/web/admin/sections.jsp
@@ -0,0 +1,68 @@
+<%@page import="icms_servlet.*;" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+  "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Admin > Sections</title>
+    <link href="/icms-war/stylesheets/style.css" rel="stylesheet" type="text/css" media="screen" />
+    <script type="text/javascript" src="/icms-war/tinymce/jscripts/tiny_mce/tiny_mce.js"></script>
+    <script type="text/javascript">
+      tinyMCE.init({
+        // General options
+        mode : "textareas",
+        theme : "advanced",
+        languages : 'fr',
+        plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount",
+
+        // Theme options
+        theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
+        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_statusbar_location : "bottom",
+        theme_advanced_resizing : true
+
+      });
+    </script>
+  </head>
+  <body>
+    <jsp:include page="../header.jsp" />
+
+    <jsp:include page="admin_menu.jsp" />
+
+    <h2>Sections</h2>
+
+    <div id="tinyMCE">
+      <h3>Nouvelle section</h3>
+      <form method="post" action="/icms-war/admin/sections">
+        <input type="hidden" id="action" name="action" value="<%= Config.CREATE%>" />
+     
+         <label for="section">Cat&eacute;gorie :</label>
+                <select name="category" title="category" id="category">
+                    <option value="">Choisissez une cat&eacute;gorie</option>
+                    <c:forEach var="u" items="${requestScope['listeCategories']}">
+                        <option value="${u.title}">${u.title}</option>
+                    </c:forEach>
+                </select>
+                <br />
+       <label for="title">Titre de la section :</label>
+        <input type="text" id="title" name="title">
+        <br />
+        <label for="permalnk">Permalink :</label>
+        <input type="text" id="permalink" name="permalink"> (auto-g&eacute;n&eacute;r&eacute; par d&eacute;faut)
+        <br />
+        <label for="intro">Intro :</label>
+        <textarea id="intro" name="intro" rows="5" cols="80"></textarea>
+        <br />
+        <label for="content">Text :</label>
+        <textarea id="content" name="content" rows="20" cols="80"></textarea>
+        <input type="submit" name="save" value="Valider" />
+      </form>
+    </div>
+
+    <jsp:include page="../footer.jsp" />
+  </body>
+</html>
